d230ee8ab35b67e30c9152e7d4f6bca77f2186b8
Samalla1974 <94819969+Samalla1974@users.noreply.github.com>
2021-12-21 08:25:27 +0530

    Update Line_Computation_OOPS.java

--- Line_Computation_OOPS.java
+++ Line_Computation_OOPS.java
@@ -5,12 +5,11 @@ package usecases;
 	{
 		public static void main(String[] args) 
 		{
-			//line 1
-			// point 1
+			
 			double x1 = 2;
 			double y1 = 2;
 
-			// point 2
+		
 			double x2 = 6;
 			double y2 = 6;
 
@@ -19,12 +18,11 @@ package usecases;
 			length1 = Math.sqrt((x2 - x1) * (x2 - x1) + (y2 - y1)* (y2 - y1));
 			System.out.println("length of a line1 :" + length1);
 		
-			//line 2
-	                // point 1
+		
 	                double lx1 = 8;
 	                double ly1 = 8;
 
-	                // point 2
+	  
 	                double lx2 = 5;
 	                double ly2 = 5;
 
